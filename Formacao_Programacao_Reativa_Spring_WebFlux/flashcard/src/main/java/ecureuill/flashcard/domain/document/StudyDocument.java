0b69c0b06d04e5d460a18073f993e2118de09360
Camilla <logikasciuro@gmail.com>
2023-07-22 06:00:49 +0000

    🐛 Fix collection name

--- Formacao_Programacao_Reativa_Spring_WebFlux/flashcard/src/main/java/ecureuill/flashcard/domain/document/StudyDocument.java
+++ Formacao_Programacao_Reativa_Spring_WebFlux/flashcard/src/main/java/ecureuill/flashcard/domain/document/StudyDocument.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.Builder;
 
-@Document(collection = "study_decks")
+@Document(collection = "studies")
 public record StudyDocument(
     @Id
     String id,
